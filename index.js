b64860ca77f7dc28b916f0f4001ce1716af3c34c
jvarghe <vcs.inbox@outlook.com>
2023-03-15 17:45:38 +0000

    'index.js': Turned existing code into comments in preparation to convert the project into OOP-style code.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-// Arrays to keep track of each task's state
+/* CHALLENGE
+ *
+ * In this activity we are going to practice using simple JavaScript objects
+ * while refactoring a Todo List command-line app.
+ *
+ * The code below is written in procedural format. Convert it to Object-oriented
+ * programming style.
+ */
+
+
+/* // TODO LIST WRITTEN IN PROCEDURAL STYLE
+
+Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
 
@@ -21,6 +33,7 @@ function logTaskState(taskIndex) {
   console.log(`${title} has${complete ? " " : " not "}been completed`);
 }
 
+
 // DRIVER CODE BELOW
 
 newTask("Clean Cat Litter"); // task 0
@@ -29,3 +42,7 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+
+*/
+
+
